e47b4c7d7244be967b2e374ca02350c9aea0c724
Victoria <58699190+procrastinatress@users.noreply.github.com>
2020-08-14 20:12:47 +1000

    Add files via upload

---
+++ loop-heart.js
@@ -0,0 +1,19 @@
+for (let index = 0; index <= 4; index++) {
+    basic.showLeds(`
+        # # . # #
+        # # # # #
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+basic.forever(function () {
+	
+})
